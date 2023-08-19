850ae5af942ab81627b6e931132d4d6efcaa2036
David Rocha <manoeudavi3@gmail.com>
2023-08-18 21:58:39 -0300

    arrumado as instruções

--- Formulario/maratona_formulario/src/componentes/InicialInformation.jsx
+++ Formulario/maratona_formulario/src/componentes/InicialInformation.jsx
@@ -1,29 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
 import { AiOutlineUser, AiOutlineCode, AiOutlineTeam } from "react-icons/ai";
 import { FiSend } from "react-icons/fi";
 const InicialInformation = () => {
   return (
     <div>
       <div className="review_form">
-        <h2 className="etapas">Etapas </h2>
+        <h2 className="etapas">Instruções </h2>
         <div className="inconesContainer">
           <div className="incones">
             <AiOutlineCode />
           </div>
           <label htmlFor=""> 
-            Na etapa inicial, é possível se inscrever nos cursos de JavaScript, Dart/Flutter ou em ambos. Para participar da Maratona de Programação - 2023, basta prosseguir clicando no botão Avançar.
+            Na etapa inicial, você pode se inscrever nos cursos de JavaScript, Dart/Flutter ou em ambos. Caso queira participar da Maratona de Programação - 2023, basta prosseguir clicando no botão "Avançar".
            </label>
         </div>
         <div className='inconesContainer'>
           <div className="incones">
             <AiOutlineTeam />
           </div>
-          <label htmlFor="">Nesta fase, você pode se inscrever na maratona. Se você é o representante da equipe, crie um novo time especificando o nome. Se não for o representante, selecione sua equipe da lista e continue. Se estiver sem equipe, escolha a opção do Select e prossiga. </label>
+          <label htmlFor="">Nesta etapa, você pode se inscrever na maratona. Se você é o representante da equipe, crie um novo time especificando o nome. Se não for o representante, selecione sua equipe da lista e continue. Se estiver sem equipe, escolha a opção "Desejo seguir a inscrição sem equipe" . </label>
         </div >
         <div className='inconesContainer'>
           <div className="incones">
             <AiOutlineUser />
           </div>
-          <label htmlFor="">Nesta fase, insira seus dados para continuar com a inscrição. </label>
+          <label htmlFor="">Nesta etapa, insira seus dados para continuar com a inscrição. </label>
         </div>
         <div className='inconesContainer'>
           <div className="incones">
