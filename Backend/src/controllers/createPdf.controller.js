fe9b88872eb5b87f25891b062491508f32f2d79a
David Rocha <manoeudavi3@gmail.com>
2023-08-19 23:25:06 -0300

    trocado biblioteca de conversão de pdf

--- Backend/src/controllers/createPdf.controller.js
+++ Backend/src/controllers/createPdf.controller.js
@@ -1,26 +1,33 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const util = require('util');
 const html = require ("../views/createPdfTemplate")
-const pdf = require('html-pdf');
+const writeFile = util.promisify(fs.writeFile);
 
-// Opções de configuração para a biblioteca html-pdf
-const options = {
-    format: 'A4',
-    orientation: 'portrait',
+const pdfTransporter = async function (nome, cpf, email, cursoFaculdade, periodoFaculdade, faculdadeNome, nomeEquipe) {
+  const pdfPath = `../pdf/confirmacao_de_inscrição_${email}.pdf`;
+
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Gere o conteúdo HTML que deseja converter em PDF
+    const htmlContent = html(nome, cpf, email, cursoFaculdade, periodoFaculdade, faculdadeNome, nomeEquipe);
+
+    await page.setContent(htmlContent);
+    await page.pdf({
+      path: pdfPath,
+      format: 'A4',
+      margin: { top: '20px', right: '20px', bottom: '20px', left: '20px' },
+    });
+
+    await browser.close();
+    console.log(`PDF criado em: ${pdfPath}`);
+    return pdfPath;
+  } catch (error) {
+    console.error(`Erro ao criar o PDF: ${error}`);
+    throw error;
+  }
 };
 
-const pdfTransporter = function (nome, cpf, email, cursoFaculdade, periodoFaculdade, faculdadeNome, nomeEquipe) {
-    return new Promise((resolve, reject) => {
-      const pdfPath = `../pdf/confirmacao_de_inscrição_${email}.pdf`; 
-  
-      pdf.create(html(nome, cpf, email, cursoFaculdade, periodoFaculdade, faculdadeNome, nomeEquipe), options).toFile(pdfPath, function (err, res) {
-        if (err) {
-          console.error(`Erro ao criar o PDF: ${err}`);
-          reject(err);
-          return;
-        }
-        console.log(`PDF criado em: ${res.filename}`);
-        resolve(pdfPath);
-      });
-    });
-  };
-
-module.exports = pdfTransporter
+module.exports = pdfTransporter;
